a4b8e2c393abef1b7758958c1e19a64a67bfffcf
denevB <95876310+denevB@users.noreply.github.com>
2023-09-29 12:35:56 +0300

    origin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import env from 'dotenv'
 
 export default defineConfig({
 	plugins: [sentrySvelteKit({
         sourceMapsUploadOptions: {
             org: "100-35",
-            project: "fragment-bg"
+            project: "fragment-bg",
+            authToken: process.env.SENTRY_AUTH_TOKEN,
         }
     }), sveltekit()]
 });
